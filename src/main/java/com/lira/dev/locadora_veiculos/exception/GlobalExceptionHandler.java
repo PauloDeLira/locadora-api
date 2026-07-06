6c2c5e237baee90d90a58e67ba178e110994655b
Paulo Lira <paulinho_santos_2001@hotmail.com>
2026-07-06 18:23:40 -0300

    feat: adiciona outra exceção personalizada ao GlobalExceptionHandler

--- src/main/java/com/lira/dev/locadora_veiculos/exception/GlobalExceptionHandler.java
+++ src/main/java/com/lira/dev/locadora_veiculos/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.lira.dev.locadora_veiculos.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -15,4 +16,25 @@ public class GlobalExceptionHandler {
         ErrorResponse erro = new ErrorResponse(ex.getMessage(),404, LocalDateTime.now());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
     }
+
+    @ExceptionHandler(VeiculoBadRequestException.class)
+    public ResponseEntity<ErrorResponse> veiculoBadRequestHandler(VeiculoBadRequestException ex){
+
+        ErrorResponse erro = new ErrorResponse(ex.getMessage(),400, LocalDateTime.now());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> MethodArgumentNotValidHandler(MethodArgumentNotValidException exception){
+
+        String msg = exception.getBindingResult()
+                .getFieldErrors().stream()
+                .map(e -> e.getField() + ": " + e.getDefaultMessage())
+                .collect(java.util.stream.Collectors.joining(", "));
+
+        VeiculoBadRequestException minhaExcecao = new VeiculoBadRequestException(msg);
+
+        return veiculoBadRequestHandler(minhaExcecao);
+    }
+
 }
---
+++ src/main/java/com/lira/dev/locadora_veiculos/exception/VeiculoBadRequestException.java
@@ -0,0 +1,7 @@
+package com.lira.dev.locadora_veiculos.exception;
+
+public class VeiculoBadRequestException extends RuntimeException {
+    public VeiculoBadRequestException(String message) {
+        super(message);
+    }
+}
